22bc9ced6c8f20d10a1383dfd95e7c335ecdff1d
Mark Chard <machard.1984@gmail.com>
2020-08-09 13:24:52 +0100

    Update CreateBaseImages.groovy

--- CreateBaseImages.groovy
+++ CreateBaseImages.groovy
@@ -16,10 +16,10 @@ def generateStage(job){
             sh "sudo rm -rf ${WORKSPACE}/${job}/var/cache/*"
         }
         stage("import ${job} image") {
-            sh "tar -C ${WORKSPACE}/${job} -c . | docker import - 127.0.0.1:5000/${job}:latest"
+            sh "tar -C ${WORKSPACE}/${job} -c . | docker import - 127.0.0.1:5000/${job}:${VERSION}"
         }
         stage("push ${job} image to cluster local repository") {
-            sh "docker image push 127.0.0.1:5000/${job}:${${job}_version}"
+            sh "docker image push 127.0.0.1:5000/${job}:${VERSION}"
         }
     }
 }
