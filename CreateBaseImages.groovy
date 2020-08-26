513a4f460cac0e7fec3016915a08cad9bc04e06b
Mark Chard <machard.1984@gmail.com>
2020-08-26 10:01:07 +0100

    Update CreateBaseImages.groovy

--- CreateBaseImages.groovy
+++ CreateBaseImages.groovy
@@ -49,9 +49,7 @@ pipeline{
                     sh "wget --proxy=${PROXY} ${CENTOS_URL}/${CENTOS_RELEASE}"
                     sh "sudo rpm -ivh --force --root=${WORKSPACE}/centos --nodeps ${CENTOS_RELEASE}"
                     sh "sudo yum --installroot=${WORKSPACE}/centos --noplugins --nogpgcheck --releasever=7 install -y yum yum-plugin-ovl"
-                }
-                dir('centos/etc/yum.repos.d/'){
-                    sh "ls -l"
+                    sh "cd etc/yum.repos.d/"
                     sh "sudo rm -f *.repo"
                     sh "sudo wget http://10.0.33.55:8081/repository/configs/yum.repos.d/CentOS.repo"
                 }
