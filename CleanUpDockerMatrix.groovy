d305cd883cdfa5c4da2e16b5ca7f26e0c7bf7ee6
Mark Chard <machard.1984@gmail.com>
2021-01-07 19:47:09 +0000

    Update CleanUpDockerMatrix.groovy

--- CleanUpDockerMatrix.groovy
+++ CleanUpDockerMatrix.groovy
@@ -3,28 +3,27 @@ pipeline {
     stages {
         stage("foo") {
             matrix {
-                node {
-                    label "${NODE}"
-                }
-                axes {
-                    axis {
-                        name 'FUNCTION'
-                        values "container", "volume", "image", "network"
-                    }
-                    axis {
-                        name 'NODE'
-                        values "rpi2", "predator", "tristram"
-                    }
-                }
-                stages {
-                    stage("Clean up docker") {
-                        steps {
-                            sh 'docker ${FUNCTION} prune -f'
+                node("${NODE"}){
+                    axes {
+                        axis {
+                            name 'FUNCTION'
+                            values "container", "volume", "image", "network"
+                        }
+                        axis {
+                            name 'NODE'
+                            values "rpi2", "predator", "tristram"
                         }
                     }
-                    stage("Show whats left") {
-                        steps {
-                            sh 'docker ${FUNCTION} ls'
+                    stages {
+                        stage("Clean up docker") {
+                            steps {
+                                sh 'docker ${FUNCTION} prune -f'
+                            }
+                        }
+                        stage("Show whats left") {
+                            steps {
+                                sh 'docker ${FUNCTION} ls'
+                            }
                         }
                     }
                 }
